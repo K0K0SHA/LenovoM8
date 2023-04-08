5c30cb97c57f9eded635b03e59acc0a7686d2859
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-04-08 04:52:28 -0400

    autorun.py still requires more work to run
    Added the ability to attempt to download the miscX repo from this github

--- autorun.py
+++ autorun.py
@@ -1,7 +1,32 @@
+# critical imports
 import os
 import platform
+import subprocess
+
+def check_dependencies():
+    if ()
+    else:
+        print("Your device does not have the required dependencies to fully automate the Lenovo Tab M8 firmware download.") 
+        return false
+def inquire(prompt):
+    user_input = input(prompt + "\ntype Y to confirm)
+    bool_input = (input.lower() == 'y')
+    return bool_input
+
+# try downloading miscX with user permission
+try:
+    import miscX
+except ImportError:
+    if inquire("Cannot import helper Python module miscX, attempt git clone from github at...\nhttps://github.com/K0K0SHA/miscX \n...? (Y/N)"):
+        cmd = ("git clone --force https://github.com/K0K0SHA/miscX")
+        os.execute(cmd)
+    if ! os.path.exists("miscX.py"):
+        if inquire("Cannot import helper Python module miscX; git clone failed. Would you like to attempt download with wget?"):
+            cmd = ("wget https://github.com/K0K0SHA/miscX")
+            os.execute(cmd)
+    
 
 if (platform.System=='Windows'):
-    os.execute("windows_config.bat")
+    os.execute(".\windows_config.bat")
 elif (platform.os=='Linux'):
-    os.execute("linux_setup.sh")
+    os.execute("./linux_setup.sh")
